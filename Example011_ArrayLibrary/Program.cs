68e037c1a435e6b43bd06b1b9103d349397ab4da
Alyona Koval <alyona-it-koval@yandex.ru>
2023-02-03 15:31:43 +0500

    Итерация2 с решением Задачи - Имеется одномерный массив array из n элементов, требуется найти элемент массива, равный n

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -1,7 +1,9 @@
-﻿// А сейчас перепишем этот код с использованием генератора псевдослучайных чисел с использованием методов. 
+﻿// Итерация 1. А сейчас перепишем этот код с использованием генератора псевдослучайных чисел с использованием методов. 
 // Мы потренируем то, каким образом можно взять, например, метод, передать в него массив и заполнить массив нужным 
 // количеством элементов. На следующем этапе опишем метод, который будет выводить все элементы по порядку. 
 // Затем превратим наш код поиска нужного индекса в метод.
+
+// Итерация 2. Имеется одномерный массив array из n элементов, требуется найти элемент массива, равный find
 
 void FillArray(int[] collection)
 {
@@ -22,12 +24,38 @@ void PrintArray(int[] col)
   while (position < count)
   {
     Console.WriteLine(col[position]);
-    position++; 
+    position++;
   }
 
 }
 
+
+int IndexOf(int[] collection, int find)
+{
+  int count = collection.Length;
+  int index = 0;
+  int position = -1;
+
+  while (index < count)
+  {
+    if (collection[index] == find)
+    {
+      position = index;
+      break;
+    }
+    index++;
+  }
+  return position;
+}
+
+
 int[] array = new int[10];
 
-FillArray(array); 
-PrintArray(array); 
+FillArray(array);
+array[4] = 4;
+array[6] = 4;
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 444);
+Console.WriteLine(pos);
